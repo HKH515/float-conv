1fe3437531496fd1271dcad627b7597b6609ca32
Hannes Hannesson <hannes@Hanness-MacBook.local>
2015-09-23 15:07:51 +0000

    fixed logic flaws

--- float.py
+++ float.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import sys
 from fractions import Fraction
 
 class fpo():
@@ -9,6 +10,12 @@ class fpo():
             self.data['sign'] = binary[0]
             self.data['exp'] = binary[1]
             self.data['frac'] = binary[2]
+        elif len(rawData.split("/")) == 2:
+            fractional = rawData.split("/")
+            self.data['upper'] = fractional[0]
+            self.data['lower'] = fractional[1]
+        elif len(rawData.split(" ")) == 0 & len(rawData.split("/")) == 0:
+            self.data['decimal'] = rawData
 
     def __binary_to_decimal(self, binaryString):
         number = 0
@@ -28,16 +35,34 @@ class fpo():
             counter += 1
         return number
 
+    def __decimal_to_binaryfrac(self, decimal):
+        binary = 0
+        counter = 1
+
+    def __decimal_to_binary(self, decimal):
+        pass
+        leftSide = decimal.round()
+        rightSide = decimal - leftSide;
+
+    def print_binary(self):
+        """
+        Converts a human readable decimal number or fraction to an IEEE 754 representation of a floating point number
+        """
+        pass
+
     def print_decimal(self):
         """
-        Converts a IEEE 754 representation of a floating point number to a human readable decimal number
+        Converts an IEEE 754 representation of a floating point number to a human readable decimal number or fraction
         """
-        normalized = 0
+        normalized = None
         frac = self.__binaryfrac_to_decimal(self.data['frac'])
         E = self.__binary_to_decimal(self.data['exp'])
         bias = 2**(len(self.data['exp'])-1)-1
-        e = E - bias
-        if e >= 0:
+        if E == 0:
+            e = 1 - bias
+            normalized = 0
+        else:
+            e = E - bias
             normalized = 1
         mantissa = frac+normalized
 
@@ -55,6 +80,11 @@ class fpo():
 
 
 if __name__ == "__main__":
-    myInput = raw_input()
+    if len(sys.argv) == 4:
+        myInput = "%s %s %s" % (sys.argv[1], sys.argv[2], sys.argv[3])
+    elif len(sys.argv) == 2:
+        myInput = sys.argv[2]
+    else:
+        myInput = raw_input()
     myObj = fpo(myInput)
     myObj.print_decimal()
